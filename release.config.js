5add0914c0c7f969e8fe5b8e94c7e12cd5050efb
Koy <koykub333@hotmail.com>
2022-07-06 16:49:21 -0600

    fix: fix description

---
+++ release.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  branches: "master",
+  repositoryUrl: "https://github.com/koykub333/react-app-test",
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/github'
+  ]
+}
